0380bc422e47f52a92bcc90bd9e71e4cb9dc1a84
ericweidman <ericweidman@gmail.com>
2017-06-11 21:14:04 -0400

    Initial import

---
+++ src/main/java/com/IHGStatTracker/IhgStatTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.IHGStatTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhgStatTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhgStatTrackerApplication.class, args);
+	}
+}
